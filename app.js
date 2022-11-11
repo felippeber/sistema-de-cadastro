8be646d971a5963f3b2c6feb82ba40372d851594
Felippe Bernardo da Silva <felippeber@gmail.com>
2022-11-11 16:38:37 -0300

    voltado para o estado do Felippe

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 // mais um consle log de teste:
 console.log("O Felippe fez esse commit sem você ver na aula!")
-console.log("Enviando para o GitHub")
-
